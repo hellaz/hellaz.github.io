92d682445078b28f64f3f3c6ece599b46ee80463
hellaz <50288964+hellaz@users.noreply.github.com>
2021-12-07 14:23:01 +0200

    update rss

--- js/init.js
+++ js/init.js
@@ -1,19 +1,19 @@
 jQuery(function ($) {
-  $("#rss-books").rss("http://feeds.feedburner.com/HellazBooks", 
+  $("#rss-books").rss("https://www.techmeme.com/feed.xml", 
    {
     limit: 5
   })
 })
 
 jQuery(function ($) {
-  $("#rss-movies").rss("http://feeds.feedburner.com/HellazMovies", 
+  $("#rss-movies").rss("https://news.google.com/rss?hl=el&gl=GR&ceid=GR:el", 
    {
     limit: 5
   })
 })
 
 jQuery(function ($) {
-  $("#rss-scripts").rss("http://feeds.feedburner.com/HellazScripts", 
+  $("#rss-scripts").rss("http://feeds.bbci.co.uk/news/rss.xml", 
    {
     limit: 5
   })
