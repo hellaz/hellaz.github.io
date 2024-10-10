5b6f9340ca5fb727ac568b2d3e201fdf032b19bf
hellaz <50288964+hellaz@users.noreply.github.com>
2024-10-10 22:26:51 +0300

    Update init.js
    RSS update

--- start/js/init.js
+++ start/js/init.js
@@ -1,19 +1,19 @@
 jQuery(function ($) {
-  $("#rss-news").rss("https://portal.hellaz.eu/rss.xml?category=14", 
+  $("#rss-news").rss("https://hellaz.net/freshrss/p/api/query.php?user=hellaz&t=09e4265e0c54a95855af01acf19c074c&f=rss", 
    {
     limit: 5
   })
 })
 
 jQuery(function ($) {
-  $("#rss-greece").rss("https://portal.hellaz.eu/rss.xml?category=2", 
+  $("#rss-greece").rss("https://hellaz.net/freshrss/p/api/query.php?user=hellaz&t=cddbcfbd6dd23c009c93721060d358f2&f=rss", 
    {
     limit: 5
   })
 })
 
 jQuery(function ($) {
-  $("#rss-sports").rss("https://portal.hellaz.eu/rss.xml?category=5", 
+  $("#rss-sports").rss("https://hellaz.net/freshrss/p/api/query.php?user=hellaz&t=ac3684161f5d666c3a93ca24fd094224&f=rss", 
    {
     limit: 5
   })
